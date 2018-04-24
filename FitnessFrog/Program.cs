c8034fa7868cdc48814bff1982a5fb164c63fe0d
Kate Chirkina <milightning.ua@gmail.com>
2018-04-24 13:12:06 +0300

    changed message, when app is quit

--- FitnessFrog/Program.cs
+++ FitnessFrog/Program.cs
@@ -66,7 +66,7 @@ namespace FitnessFrog
                 // Repeat until user quits
             }
 
-            Console.WriteLine("Goodbye");
+            Console.WriteLine("Bye-bye");
         }
     }
 }
